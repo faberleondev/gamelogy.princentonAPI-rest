7c80296a60f46e50a93cf13d1173ab0633f9895d
Fabricio Sebastían León <129199337+faberleondev@users.noreply.github.com>
2025-01-18 21:53:19 -0300

    Inaugurando los Commit

---
+++ src/main/java/com/gamelogy/princenton/PrincentonApplication.java
@@ -0,0 +1,13 @@
+package com.gamelogy.princenton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincentonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincentonApplication.class, args);
+	}
+
+}
